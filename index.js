341944ec42b931c1e14c351bc93ea2dee5797ba2
yasin-sorkar47 <mdyacinsorkar17@gmail.com>
2024-12-04 23:39:59 +0600

    add delete feature

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete the movie
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await moviesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
